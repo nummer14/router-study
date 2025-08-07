e1c137e5e8eab8927d17ac6e3f711f5a41b0cf9c
nummer14 <nummer14@naver.com>
2025-08-07 15:21:41 +0900

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
+import Header from "./components/Header";
+import User from "./pages/user";
+import Users from "./pages/users";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/user/:id" element={<User />} />
       </Routes>
     </BrowserRouter>
   );
