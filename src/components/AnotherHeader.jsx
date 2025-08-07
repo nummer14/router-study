323fb4db2638dca5ff90292e3af3ca89ce2bdaa9
nummer14 <nummer14@naver.com>
2025-08-07 15:44:29 +0900

    another layout

---
+++ src/components/AnotherHeader.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+const AnotherHeader = () => {
+  return (
+    <header className="bg-cyan-300 flex justify-between items-center px-20">
+      <h1>AnotherHeader</h1>
+      <nav className="flex gap-4">
+        <Link className="bg-red-300 px-4 py-2 rounded" to="/">
+          Home
+        </Link>
+        <Link className="bg-blue-300 px-4 py-2 rounded" to="/about">
+          About
+        </Link>
+        <Link className="bg-orange-300 px-4 py-2 rounded" to="/users">
+          Users
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default AnotherHeader;
